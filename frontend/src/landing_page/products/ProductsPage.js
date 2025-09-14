435bd560118c6e3cedcc9146bb0386e147ebbb62
varunnevuri <varunnevuri198@gmail.com>
2025-09-15 00:41:51 +0530

    Updated frontend (UI changes, assets, new components)

---
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from './Hero';
+import LeftSection from './LeftSection';
+import RightSection from './RightSection';
+import Universe from './Universe';
+function ProductsPage() {
+    return ( 
+        <>
+        <Hero/>
+        <LeftSection/>
+        <RightSection/>
+        <Universe/>
+        </>
+     );
+}
+
+export default ProductsPage;
